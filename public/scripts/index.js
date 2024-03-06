68dfca746e7998bf16ecbd6315f2bac95cfc049e
mohitbaraiya <mohitbaraiya666@gmail.com>
2024-03-06 11:59:16 +0530

    FEAT : socket event setup

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,15 +1,54 @@
 import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js";
+
+let isAlreadyCalling = false;
+let getCalled = false;
+
+const existingCalls = [];
+
+const { RTCPeerConnection, RTCSessionDescription } = window;
+
+const peerConnection = new RTCPeerConnection();
+
 navigator.getUserMedia(
   { video: true, audio: true },
   (stream) => {
     const localVideo = document.getElementById("local-video");
     if (localVideo && stream) {
       localVideo.srcObject = stream;
+      stream
+        .getTracks()
+        .forEach((track) => peerConnection.addTrack(track, stream));
     }
   },
   (error) => console.error(error)
 );
 
+function unselectUsersFromList() {
+  const alreadySelectedUser = document.querySelectorAll(
+    ".active-user.active-user--selected"
+  );
+
+  alreadySelectedUser.forEach((el) => {
+    el.setAttribute("class", "active-user");
+  });
+}
+
+peerConnection.ontrack = function ({ streams: [stream] }) {
+  const remoteVideo = document.getElementById("remote-video");
+  if (remoteVideo) {
+    remoteVideo.srcObject = stream;
+  }
+};
+
+async function callUser(socketId) {
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
+
+  socket.emit("call-user", {
+    offer,
+    to: socketId,
+  });
+}
 function createUserItemContainer(socketId) {
   const userContainerEl = document.createElement("div");
 
@@ -54,5 +93,29 @@ socket.on("remove-user", ({ socketId }) => {
 
   if (elToRemove) {
     elToRemove.remove();
+  }
+});
+
+socket.on("call-made", async (data) => {
+  await peerConnection.setRemoteDescription(
+    new RTCSessionDescription(data.offer)
+  );
+  const answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+
+  socket.emit("make-answer", {
+    answer,
+    to: data.socket,
+  });
+});
+
+socket.on("answer-made", async (data) => {
+  await peerConnection.setRemoteDescription(
+    new RTCSessionDescription(data.answer)
+  );
+
+  if (!isAlreadyCalling) {
+    callUser(data.socket);
+    isAlreadyCalling = true;
   }
 });
--- src/server.ts
+++ src/server.ts
@@ -54,6 +54,21 @@ export class Server {
           users: [socket.id],
         });
       }
+
+      socket.on("call-user", (data) => {
+        socket.to(data.to).emit("call-made", {
+          offer: data.offer,
+          socket: socket.id,
+        });
+      });
+
+      socket.on("make-answer", (data) => {
+        socket.to(data.to).emit("answer-made", {
+          socket: socket.id,
+          answer: data.answer,
+        });
+      });
+
       socket.on("disconnect", () => {
         this.#activeSockets = this.#activeSockets.filter(
           (existingSocket) => existingSocket !== socket.id
