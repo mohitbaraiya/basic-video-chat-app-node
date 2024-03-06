ec03ca118c1a6b62383056f5f75803bf785964a5
mohitbaraiya <mohitbaraiya666@gmail.com>
2024-03-06 11:31:48 +0530

    FEAT : socket setup

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,3 +8,26 @@ navigator.getUserMedia(
   },
   (error) => console.error(error)
 );
+import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js";
+
+const socket = io.connect();
+
+socket.on("connection", (socket) => {
+  const existingSocket = this.activeSockets.find(
+    (existingSocket) => existingSocket === socket.id
+  );
+  console.log({ existingSocket });
+  if (!existingSocket) {
+    this.activeSockets.push(socket.id);
+
+    socket.emit("update-user-list", {
+      users: this.activeSockets.filter(
+        (existingSocket) => existingSocket !== socket.id
+      ),
+    });
+
+    socket.broadcast.emit("update-user-list", {
+      users: [socket.id],
+    });
+  }
+});
